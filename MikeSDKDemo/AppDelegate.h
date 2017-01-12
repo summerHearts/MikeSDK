e6691006c6696c7b21c084ca2c1fc5e2bdb120ca
kenvin <summerHearts@163.com>
2017-01-12 20:01:02 +0800

    Initial Commit

---
+++ MikeSDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MikeSDKDemo
+//
+//  Created by Kenvin on 17/1/12.
+//  Copyright © 2017年 上海方创金融信息服务股份有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MikeSDKDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MikeSDKDemo
+//
+//  Created by Kenvin on 17/1/12.
+//  Copyright © 2017年 上海方创金融信息服务股份有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
